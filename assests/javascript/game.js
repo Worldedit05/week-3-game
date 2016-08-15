4936fb6601de1732264dfd85bb0b97adf30f28e0
Aaron Lee <worldedit05@gmail.com>
2016-08-15 00:09:30 -0500

    Answers will blank out

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -4,7 +4,17 @@ var start = {
 	chooseWord: function(){
 
 		var answerWord = start.words[Math.floor(Math.random()*start.words.length)];
+		
 		console.log(answerWord);
+
+		var blankAnswers = "";
+
+		for (var i = 0; i < answerWord.length; i++) {
+			
+			var blankAnswers = blankAnswers.concat(" _ ");
+
+		}
+		console.log(blankAnswers);
 	}
 }
 
