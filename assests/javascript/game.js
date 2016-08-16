fd0889f8fb0d73c519c10adb2eaf3fb20597a337
Aaron Lee <worldedit05@gmail.com>
2016-08-16 02:35:22 -0500

    Completed logic for guessing and returning the correct string

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -1,37 +1,64 @@
 // Choose a random word from the list and provide blank spaces
 
-var start = {
+var game = {
 	words: ['halo','destiny','league of legends','hearthstone','overwatch'],
 
-	chooseWord: function(){
+	chooseWord: function(answerWord){
 
-		var answerWord = start.words[Math.floor(Math.random()*start.words.length)];
-		
-		console.log(answerWord);
-
-		var blankAnswers = "";
+		var blankCharacter = "";
 
 		for (var i = 0; i < answerWord.length; i++) {
 			
-			var blankAnswers = blankAnswers.concat(" _ ");
+			var blankCharacter = blankCharacter.concat("_");
 
 		}
-		console.log(blankAnswers);
+		console.log(blankCharacter);
+
+		return blankCharacter;
 	},
 
 	setChar: function( currentWord, x, y ){
 
-		var repWord = currentWord.substr(0, x) + y + currentWord.substr(x + 1);
-
-		console.log(repWord);
+		var repWord = currentWord.substr(0, x) + y + currentWord.substr(x + 1, currentWord.length);
 
 		return repWord;
+	},
+
+	guessLetter: function( letter, shown, answer ){
+
+		var checkLetter = -1;
+
+		checkLetter = answer.indexOf(letter);
+
+		for (i = 0; i < answer.length; i++) {
+		
+			if (checkLetter == -1){
+
+				return shown;
+
+			}
+			else{
+				
+				var shown = game.setChar(shown, checkLetter, letter);
+
+			}
+
+			checkLetter = answer.indexOf(letter, checkLetter + 1);
+
+		}
+
+		return shown;
+
 	}
 }
 
-start.chooseWord();
+var answerWord = game.words[Math.floor(Math.random()*game.words.length)];
 
-start.setChar("Test words", 3, "6");
+var currentWord = game.chooseWord(answerWord);
+
+var numGuesses = answerWord.length + 3;
+
+console.log(answerWord);
 
 // Listen for the user's guess
 
@@ -39,6 +66,25 @@ document.onkeyup = function(event){
 
 	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
+	console.log("Number of guesses: " + numGuesses)
 
+	if( answerWord.indexOf(userGuess) >= 0 ){
+
+		var newWord = game.guessLetter(userGuess, currentWord, answerWord);
+
+	}
+	else{
+
+		var newWord = currentWord;
+
+		console.log("Letter did not match");
+
+	}
+
+	numGuesses = numGuesses - 1;
+
+	currentWord = newWord;
+
+	console.log(currentWord);
 }
 
